cfcad212cb1c5735e943fae0a503c8b85f24a34d
Trey Hayden <treyhay@gmail.com>
2016-09-18 22:03:31 -0400

    first stab at the player profile model

---
+++ src/SoccerRadarAPI/Models/Players/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoccerRadarAPI.Models.Players
+{
+    public class Player
+    {
+        public Player()
+        {
+            id = string.IsNullOrEmpty(id)
+                ? Guid.NewGuid().ToString("D")
+                : id;
+        }
+
+        public Player(string playerId) : base()
+        {
+            Guid guid;
+
+            if (Guid.TryParse(playerId, out guid))
+            {
+                id = guid.ToString("D");
+            }
+        }
+
+        public List<PlayerStatistic> statistics { get; set; }
+        public List<Team> teams { get; set; }
+        public string birthdate { get; set; }
+        public string country { get; set; }
+        public string country_code { get; set; }
+        public string first_name { get; set; }
+        public string full_first_name { get; set; }
+        public string full_last_name { get; set; }
+        public string height_cm { get; set; }
+        public string height_in { get; set; }
+        public string id { get; set; } = string.Empty;
+        public string last_name { get; set; }
+        public string position { get; set; }
+        public string rank { get; set; }
+        public string reference_id { get; set; }
+        public string weight_kg { get; set; }
+        public string weight_lb { get; set; }
+        public Total total { get; set; }
+    }
+
+    public class Total
+    {
+        public Int16 matches { get; set; }
+        public Int16 goals { get; set; }
+        public Int16 substituted_in { get; set; }
+    }
+}
---
+++ src/SoccerRadarAPI/Models/Players/PlayerStatistic.cs
@@ -0,0 +1,53 @@
+﻿namespace SoccerRadarAPI.Models.Players
+{
+    public class PlayerStatistic
+    {
+        public string goals { get; set; }
+        public string goal_frequency { get; set; }
+        public string goal_average { get; set; }
+        public string goal_assists { get; set; }
+        public string matches_played { get; set; }
+        public string matches_started { get; set; }
+        public string minutes_played { get; set; }
+        public string yellow_cards { get; set; }
+        public string yellow_red_cards { get; set; }
+        public string red_cards { get; set; }
+        public string substitutions_out { get; set; }
+        public string substitutions_in { get; set; }
+        public Category category { get; set; }
+        public TournamentGroup tournament_group { get; set; }
+        public Team team { get; set; }
+        public Tournament tournament { get; set; }
+    }
+
+    public class Category
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string country_code { get; set; }
+        public string country { get; set; }
+        public string reference_id { get; set; }
+    }
+
+    public class TournamentGroup
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string reference_id { get; set; }
+    }
+
+    public class Tournament
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string season_id { get; set; }
+        public string type { get; set; }
+        public string reference_id { get; set; }
+    }
+
+    public class Season
+    {
+        public string year { get; set; }
+        public PlayerStatistic statistic { get; set; }
+    }
+}
