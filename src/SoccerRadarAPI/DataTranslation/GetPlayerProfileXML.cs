7d0212481786d6fecb200f1f446d8d12e25bd239
Trey Hayden <treyhay@gmail.com>
2016-09-22 23:43:33 -0400

    gg

--- src/SoccerRadarAPI/DataTranslation/GetPlayerProfileXML.cs
+++ src/SoccerRadarAPI/DataTranslation/GetPlayerProfileXML.cs
@@ -8,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace SoccerRadarAPI.DataTranslation
 {
-    public class GetPlayerProfileXML : IData<PlayerProfile>
+    
+    //https://api.sportradar.us/soccer-t2/eu/matches/[match_id]/boxscore.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/matches/[match_id]/summary.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/matches/[year]/[month]/[day]/boxscore.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/matches/[year]/[month]/[day]/schedule.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/matches/[year]/[month]/[day]/summary.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/matches/schedule.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/players/[player_id]/profile.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/players/leader.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/teams/[team_id]/profile.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/teams/hierarchy.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+    //https://api.sportradar.us/soccer-t2/eu/teams/standing.xml?api_key=bkkv66h478c5pcbg3mm8s3mv
+
+    public class GetXML<T>
     {
         private readonly HttpClient _http = new HttpClient();
         private const string urlPart1 = "https://api.sportradar.us/soccer-t2/eu/players/";
---
+++ src/SoccerRadarAPI/Models/XML/MatchBoxscore.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+namespace SoccerRadarAPI.Models.XML
+{
+    public class MatchBoxscore
+    {
+        [XmlRoot(ElementName = "include", Namespace = "http://www.w3.org/2001/XMLSchema")]
+        public class Include
+        {
+            [XmlAttribute(AttributeName = "schemaLocation")]
+            public string SchemaLocation { get; set; }
+        }
+
+        [XmlRoot(ElementName = "element", Namespace = "http://www.w3.org/2001/XMLSchema")]
+        public class Element
+        {
+            [XmlAttribute(AttributeName = "name")]
+            public string Name { get; set; }
+            [XmlAttribute(AttributeName = "type")]
+            public string Type { get; set; }
+            [XmlAttribute(AttributeName = "maxOccurs")]
+            public string MaxOccurs { get; set; }
+            [XmlAttribute(AttributeName = "minOccurs")]
+            public string MinOccurs { get; set; }
+        }
+
+        [XmlRoot(ElementName = "sequence", Namespace = "http://www.w3.org/2001/XMLSchema")]
+        public class Sequence
+        {
+            [XmlElement(ElementName = "element", Namespace = "http://www.w3.org/2001/XMLSchema")]
+            public List<Element> Element { get; set; }
+        }
+
+        [XmlRoot(ElementName = "attribute", Namespace = "http://www.w3.org/2001/XMLSchema")]
+        public class Attribute
+        {
+            [XmlAttribute(AttributeName = "name")]
+            public string Name { get; set; }
+            [XmlAttribute(AttributeName = "use")]
+            public string Use { get; set; }
+            [XmlAttribute(AttributeName = "type")]
+            public string Type { get; set; }
+        }
+
+        [XmlRoot(ElementName = "complexType", Namespace = "http://www.w3.org/2001/XMLSchema")]
+        public class ComplexType
+        {
+            [XmlElement(ElementName = "sequence", Namespace = "http://www.w3.org/2001/XMLSchema")]
+            public Sequence Sequence { get; set; }
+            [XmlElement(ElementName = "attribute", Namespace = "http://www.w3.org/2001/XMLSchema")]
+            public Attribute Attribute { get; set; }
+            [XmlAttribute(AttributeName = "name")]
+            public string Name { get; set; }
+            [XmlElement(ElementName = "complexContent", Namespace = "http://www.w3.org/2001/XMLSchema")]
+            public ComplexContent ComplexContent { get; set; }
+        }
+
+        [XmlRoot(ElementName = "extension", Namespace = "http://www.w3.org/2001/XMLSchema")]
+        public class Extension
+        {
+            [XmlElement(ElementName = "sequence", Namespace = "http://www.w3.org/2001/XMLSchema")]
+            public Sequence Sequence { get; set; }
+            [XmlAttribute(AttributeName = "base")]
+            public string Base { get; set; }
+        }
+
+        [XmlRoot(ElementName = "complexContent", Namespace = "http://www.w3.org/2001/XMLSchema")]
+        public class ComplexContent
+        {
+            [XmlElement(ElementName = "extension", Namespace = "http://www.w3.org/2001/XMLSchema")]
+            public Extension Extension { get; set; }
+        }
+
+        [XmlRoot(ElementName = "schema", Namespace = "http://www.w3.org/2001/XMLSchema")]
+        public class Schema
+        {
+            [XmlElement(ElementName = "include", Namespace = "http://www.w3.org/2001/XMLSchema")]
+            public Include Include { get; set; }
+            [XmlElement(ElementName = "element", Namespace = "http://www.w3.org/2001/XMLSchema")]
+            public Element Element { get; set; }
+            [XmlElement(ElementName = "complexType", Namespace = "http://www.w3.org/2001/XMLSchema")]
+            public List<ComplexType> ComplexType { get; set; }
+            [XmlAttribute(AttributeName = "xs", Namespace = "http://www.w3.org/2000/xmlns/")]
+            public string Xs { get; set; }
+            [XmlAttribute(AttributeName = "soccer", Namespace = "http://www.w3.org/2000/xmlns/")]
+            public string Soccer { get; set; }
+            [XmlAttribute(AttributeName = "targetNamespace")]
+            public string TargetNamespace { get; set; }
+            [XmlAttribute(AttributeName = "elementFormDefault")]
+            public string ElementFormDefault { get; set; }
+        }
+
+    }
+
+}
+}
